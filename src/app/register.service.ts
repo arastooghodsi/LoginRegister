b4c868b5197c17305a62d6612d219c9a2d00a7db
arastooghodsi <arastoo.ghodsi.ai@gmail.com>
2021-06-19 21:34:28 +0430

    start add to mock file

--- src/app/register.service.ts
+++ src/app/register.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { User } from './user';
 import { USERS } from './mock-users';
 import { Form } from '@angular/forms';
+import { JsonPipe } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
@@ -27,6 +28,22 @@ export class RegisterService {
       console.log(newUser);
     }
 
+  }
+
+  addUser(newUser: User) {
+    let flag = true;
+    for(let i = 0; i < USERS.length; i++) {
+        if( 
+          (JSON.stringify(newUser.email) === JSON.stringify(USERS[i]. email)) ||
+          (JSON.stringify(newUser.username) === JSON.stringify(USERS[i]. username)) ||
+          (JSON.stringify(newUser.phone) === JSON.stringify(USERS[i]. phone)))
+          flag = false;
+    }
+    if(!flag) {
+      console.log('duplicate data');
+    }else {
+      // JSON.stringify. = JSON.stringify(newUser.email)
+    }
   }
 
 }
